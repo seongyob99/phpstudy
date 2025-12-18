86742b221a02306276d2652ff3ae3a38fced3875
seongyob99 <seongyob99@naver.com>
2025-12-18 19:08:44 +0900

    목록 화면

--- index.php
+++ index.php
@@ -13,6 +13,18 @@ $todos = $pdo->query("SELECT * FROM todos ORDER BY id DESC")->fetchAll();
 </head>
 <body>
     <h2>TODO List</h2>
-    
+<form method="post" action="add.php">
+    <input name="title" required>
+    <button>추가</button>
+</form>
+<ul>
+    <?php foreach($todos as $todo):?>
+        <li>
+            <?= htmlspecialchars($todo['title']) ?>
+            <a href="toggle.php?id=<?=  $todo['id']?>">[완료]</a>
+            <a href="toggle.php?id=<?=  $todo['id']?>">[삭제]</a>
+        </li>
+<?php endforeach ?>
+</ul>
 </body>
 </html>
